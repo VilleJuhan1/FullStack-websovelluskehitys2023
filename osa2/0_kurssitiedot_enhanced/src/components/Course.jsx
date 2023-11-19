600462c1009ff970b76b14af3351b8cc780af2bd
Ville-Juhani Nivasalo <villejuhani.nivasalo@icloud.com>
2023-11-19 11:36:43 +0200

    First two exercises of part2, kinda

---
+++ osa2/0_kurssitiedot_enhanced/src/components/Course.jsx
@@ -0,0 +1,62 @@
+// Return the string as a h1-header
+const Header = (props) => {
+    var courseName = props.name
+    console.log("Käytiin komponentin Course osassa Header, propsit: ", props)
+    return (
+      <div>
+        <h1>{courseName}</h1>
+      </div>
+    )
+}
+
+// Return a single string-representation of the exercise name and the amount of work for it
+const Part = (props) => {
+    console.log(props)
+    return (
+      <div>
+        <p>{props.name} {props.exercises}</p>
+      </div>
+    )
+}
+
+// Iterates through every exercise in the course
+const Content = (props) => {
+    var ex = props.parts
+    console.log(ex);
+    return (
+      <div>
+        {ex.map((object, index) => (
+          <Part key={index} name={object.name} exercises={object.exercises} />
+        ))}
+      </div>
+    )
+}
+
+// Accumulate the total amount of exercises
+const Total = (props) => {
+    var ex = props.ex
+    console.log(ex)
+    const total = ex.reduce((accumulator, object) => accumulator + object.exercises, 0)
+    return (
+      <div>
+        <p>Number of exercises {total}</p>
+      </div>
+    )
+}
+
+// The main component for composing the course details together
+const Course = (props) => {
+    var course = props.course
+    console.log("Tässä käydään Course-komponentissa.")
+    console.log("Nämä tuotiin: ", course)
+
+    return (
+        <div>
+            <Header name = {course.name}/>
+            <Content parts = {course.parts}/>
+            <Total ex = {course.parts}/>
+        </div>
+    )
+}
+
+export default Course
