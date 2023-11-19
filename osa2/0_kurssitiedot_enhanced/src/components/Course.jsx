20f684c32e99591f301a06bcfb8fe8eb7ccb610e
Ville-Juhani Nivasalo <villejuhani.nivasalo@icloud.com>
2023-11-19 14:24:16 +0200

    Part 2.4 and accidentally also 2.5 fully functional

--- osa2/0_kurssitiedot_enhanced/src/components/Course.jsx
+++ osa2/0_kurssitiedot_enhanced/src/components/Course.jsx
@@ -1,10 +1,10 @@
-// Return the string as a h1-header
+// Return the string as a h2-header
 const Header = (props) => {
     var courseName = props.name
-    console.log("       Header-komponentti: ", props)
+    console.log("           Header-komponentti: ", props)
     return (
       <div>
-        <h1>{courseName}</h1>
+        <h2>{courseName}</h2>
       </div>
     )
 }
@@ -22,7 +22,7 @@ const Part = (props) => {
 // Accumulate the total amount of exercises
 const Total = (props) => {
     var ex = props.ex
-    console.log("       Total-komponentti, ", ex)
+    console.log("           Total-komponentti, ", ex)
     const total = ex.reduce((accumulator, object) => accumulator + object.exercises, 0)
     return (
       <div>
@@ -34,10 +34,14 @@ const Total = (props) => {
 // Iterates through every exercise in the course
 const Content = (props) => {
     var ex = props.parts
+    var name = props.name
     console.log("       Content-komponentti, ", ex)
     return (
       <div>
-        {ex.map((object, index) => (<Part key={index} name={object.name} exercises={object.exercises} />))}
+        <Header name = {name}/>
+        {ex.map((object, index) => (
+            <Part key={index} name={object.name} exercises={object.exercises} />
+        ))}
         <Total ex = {ex}/>
       </div>
     )
@@ -45,13 +49,14 @@ const Content = (props) => {
 
 // The main component for composing the course details together
 const Course = (props) => {
-    var course = props.course
-    console.log("   Course-komponentti, ", course)
+    var courses = props.courses
+    console.log("   Course-komponentti, ", courses)
 
     return (
         <div>
-            <Header name = {course.name}/>
-            <Content parts = {course.parts}/>
+            {courses.map((object, index) => (
+                <Content key={index} name={object.name} parts={object.parts} />
+            ))}            
         </div>
     )
 }
