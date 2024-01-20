1458ad95c4a7a288e0538cefffff1cc974a34ed3
Ville-Juhani Nivasalo <villejuhani.nivasalo@icloud.com>
2024-01-20 15:18:23 +0200

    2.9 works but two separate lists, will work on that on 2.10

--- osa2/1_puhelinluettelo/src/App.jsx
+++ osa2/1_puhelinluettelo/src/App.jsx
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '0405556666' }
-  ]);
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' },
+    { name: 'Karri Karvakoipi', phone: '666-123-1234' }
+  ])
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('')
+  const [filterByInput, setFilter] = useState('')
 
   const handleAddName = (event) => {
     console.log(event.target.value)
@@ -17,6 +22,10 @@ const App = () => {
     setNewPhone(event.target.value);
   };  
 
+  const handleFilterInputChange = () => {
+    console.log(event.target.value)
+    setFilter(event.target.value);
+  }
   const addPerson = (event) => {
     event.preventDefault();
 
@@ -38,9 +47,19 @@ const App = () => {
     )
   }
 
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filterByInput.toLowerCase())
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter: <input type="text" value={filterByInput} onChange={handleFilterInputChange} />
+        </div>
+      </form>
+      <h2>Add a new person and/or number</h2>
       <form onSubmit={addPerson}>
         <div>name: <input type="text" value={newName} onChange={handleAddName} /></div>
         <div>phone: <input type="text" value={newPhone} onChange={handleAddPhone} /></div>
@@ -50,6 +69,10 @@ const App = () => {
         {persons.map((person, index) => (
           <Name key={index} name={person.name} phone={person.phone}/>
         ))}
+      <h2>Filtered</h2>
+        {filteredPersons.map((person, index) => (
+            <li key={index}>{person.name} - {person.phone}</li>
+          ))}
     </div>
   );
 };
